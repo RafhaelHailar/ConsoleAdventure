03de4bdac95080947f99ecb6eca0584753bc8c2d
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-08-08 15:35:55 +0800

    Project Setup

---
+++ ConsoleAdventure/Game.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+
+public class Game
+{
+    protected Dictionary<string, ArrayList> locations = new Dictionary<string, ArrayList>();
+
+    protected string[,] directions =
+    {
+        { "center", "spacious living room" },
+        { "center", "wine room" },
+        { "spacious living room", "center" },
+        { "spacious living room", "family history room" },
+        { "spacious living room", "study room" },
+        { "study room", "spacious living room" },
+        { "family history room", "spacious living room" },
+        { "family history room", "wine room" },
+        { "wine room", "center" },
+    };
+
+    public Game() {
+        createLocation();
+
+        foreach (KeyValuePair<string, ArrayList> location in locations)
+        {
+            ArrayList paths = location.Value;
+            Console.Write("{0} -> ",
+                      location.Key);
+
+            for ( int i = 0; i < paths.Count; i++ )
+            {
+                Console.Write("{0}, ",paths[i]);
+            }
+
+            Console.WriteLine("");
+        }
+    }
+
+    protected void constructPath(string from, string to)
+    { 
+        bool pathExists = locations.ContainsKey(from);
+
+        if (pathExists)
+        {
+            ArrayList paths = locations[from];
+
+            paths.Add(to);
+        } else
+        {
+            ArrayList initialPaths = new ArrayList();
+            initialPaths.Add(to);
+            locations.Add(from, initialPaths);
+        }
+    }
+
+    protected void createLocation()
+    {
+        for (int i = 0; i < directions.GetLength(0); i++)
+        {
+            string from = directions[i, 0];
+            string to = directions[i, 1];
+
+            constructPath(from, to);
+        }
+    }
+}
---
+++ ConsoleAdventure/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Timers;
+using static Game;
+
+namespace ConsoleAdventure
+{
+    internal class Program  
+    {
+        private static System.Timers.Timer aTimer;
+        static string intro = "Welcome, to the game of console";
+        static int pointer = 0;
+        static int counter = 0;
+        static void Main(string[] args)
+        {
+            Game game = new Game();
+            setTimer();
+            Console.ReadLine();
+        }
+
+        private static void setTimer()
+        {
+            aTimer = new System.Timers.Timer(70);
+            aTimer.Elapsed += OnTimeEvent;
+            aTimer.Enabled = true;
+            aTimer.AutoReset = true;
+        }
+
+        private static void OnTimeEvent(Object source, ElapsedEventArgs e)
+        {
+            counter++;
+            if (pointer == intro.Length)
+            {
+                aTimer.Stop();
+                aTimer.Dispose();
+                return;
+            }
+
+            char symbol = intro[pointer];
+            Console.Write(symbol);
+
+            pointer++;
+        }
+    }
+}
