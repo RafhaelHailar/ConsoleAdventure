91b066936710b316c96e49055d8dc80916ca9277
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-08-15 23:56:00 +0800

    Feat: make choice.

--- ConsoleAdventure/Game.cs
+++ ConsoleAdventure/Game.cs
@@ -117,4 +117,26 @@ public class Game
     {
         currentChoices = choicesKeys;
     }
+
+    public static void MakeChoice(string choice)
+    {
+        if (choice == null) throw new ArgumentNullException();
+
+        string[] choices = getCurrentChoices();
+
+        bool exists = false;
+
+        for (int i = 0; i < choices.Length; i++)
+        {
+            if (choices[i].Equals(choice))
+            {
+                exists = true;
+            }
+
+        }
+
+        if (!exists) throw new Exception("choice given is not part of choices!");
+
+        Console.WriteLine("You choosed: {0}",choice);
+    }
 }
