48ecc0d29f26c7d743b4a1c88e8deebf34d4bc7a
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-08-23 23:53:43 +0800

    feat: decision tree.

---
+++ ConsoleAdventure/Decision.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+
+public class Decision
+{
+    private DecisionNode currentDecision; 
+
+    public Decision()
+    {
+        DecisionNode testPath = new DecisionNode("test path");
+        DecisionNode start = new DecisionNode("start");
+
+        start.AddBranch("go to test path", testPath);
+
+        currentDecision = start;
+    }
+
+    class DecisionNode
+    {
+        private string name;
+        private readonly Dictionary<string, DecisionNode> branches = new Dictionary<string, DecisionNode>();
+        private readonly ArrayList choices = new ArrayList();
+
+        public DecisionNode(string name)
+        {
+            this.name = name;
+        }
+
+        public void AddBranch(string name,DecisionNode node)
+        {
+            branches.Add(name, node);
+            choices.Add(name);
+        }
+    }
+}
